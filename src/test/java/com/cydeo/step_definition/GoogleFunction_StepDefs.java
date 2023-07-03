f7c45893e2a9dfc2d29822e127f92f76969058fc
Gianpaulo_Cruz <cruzgianpaulo@gmail.com>
2023-07-03 19:20:08 -0400

    Real

---
+++ src/test/java/com/cydeo/step_definition/GoogleFunction_StepDefs.java
@@ -0,0 +1,43 @@
+package com.cydeo.step_definition;
+
+import com.cydeo.pages.GoogleSearchPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+
+public class GoogleFunction_StepDefs {
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @Given("User is on the google base page")
+    public void userIsOnTheGoogleBasePage() {
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get("http://www.Google.com");
+    }
+
+    @When("User chooses to search for Tesla page")
+    public void userChoosesToSearchForTeslaPage() {
+        googleSearchPage.searchBox.sendKeys("Tesla", Keys.ENTER);
+    }
+
+    @And("User clicks onto Tesla.com page")
+    public void userClicksOntoTeslaComPage() {
+        WebElement linkWeNeed = Driver.getDriver().findElement(By.xpath("//h3[@class='LC20lb MBeuO DKV0Md']"));
+        linkWeNeed.click();
+    }
+
+    @Then("User will in the Tesla Website")
+    public void userWillInTheTeslaWebsite() {
+        String Title = Driver.getDriver().getTitle();
+        System.out.println(Title);
+        Assert.assertEquals(Title, "Electric Cars, Solar & Clean Energy | Tesla");
+        Driver.closeDriver();
+    }
+}
---
+++ src/test/resources/features/Google.feature
@@ -0,0 +1,8 @@
+@Egg
+Feature: Search Functionality for google
+
+  Scenario: Get to Tesla Page
+    Given User is on the google base page
+    When User chooses to search for Tesla page
+    And User clicks onto Tesla.com page
+    Then User will in the Tesla Website
