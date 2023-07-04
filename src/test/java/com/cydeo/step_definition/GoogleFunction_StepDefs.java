893e6f64efe9db88d935a093373e4e666c1640b7
Gianpaulo_Cruz <cruzgianpaulo@gmail.com>
2023-07-04 17:19:32 -0400

    Real

--- src/test/java/com/cydeo/step_definition/GoogleFunction_StepDefs.java
+++ src/test/java/com/cydeo/step_definition/GoogleFunction_StepDefs.java
@@ -7,6 +7,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -16,6 +18,7 @@ import org.openqa.selenium.WebElement;
 public class GoogleFunction_StepDefs {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    Logger log = LogManager.getLogger(this.getClass());
     @Given("User is on the google base page")
     public void userIsOnTheGoogleBasePage() {
         Driver.getDriver().manage().window().maximize();
